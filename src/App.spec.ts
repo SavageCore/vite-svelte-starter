1cbc6e84c1a9c00bf41ae816c6e534620e78a29a
Oliver Sayers <talk@savagecore.uk>
2023-09-21 23:19:43 +0100

    Add vitest

---
+++ src/App.spec.ts
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+
+import { render, fireEvent, screen } from '@testing-library/svelte'
+
+import App from './App.svelte'
+
+test('shows proper heading when rendered', () => {
+    render(App)
+    const heading = screen.getByText('Vite + Svelte')
+    expect(heading).toBeInTheDocument()
+})
+
+test('changes button text on click', async () => {
+    render(App)
+    const button = screen.getByRole('button')
+
+    expect(button).toHaveTextContent('count is 0')
+
+    await fireEvent.click(button)
+
+    expect(button).toHaveTextContent('count is 1')
+})
---
+++ vitest-setup.ts
@@ -0,0 +1,2 @@
+import '@testing-library/jest-dom/vitest'
+import './src/i18n.js';
